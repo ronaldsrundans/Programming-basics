dc53aea390b5797b26d17f9dd1f8127a970a3dba
Ronny <ronaldsrundans@gmail.com>
2017-09-22 20:54:28 +0300

    Update new_gen.cpp

--- automatu_teorija/new_gen.cpp
+++ automatu_teorija/new_gen.cpp
@@ -58,10 +58,10 @@ if (arr[1]==5)
 {
     sum=sum+2;
 }
-if (arr[2]==5)
+if (arr[2]>1 )
 {
     sum=sum+2;
-}                                  // c[1]=c2[arr[1]];
+}
 
  if (arr[2]==5 && arr[1]==5 )c[1]=c2[arr[1]];
 
@@ -112,10 +112,11 @@ if (arr[4]==5)
 {
     sum=sum+2;
 }
-if(arr[2]>1)
+/*
+if(arr[1]>1)
 {
     sum=sum+2;
-}
+}*/
 //sum=arr[2]+arr[3]+arr[4];
  if (arr[4]==5 && arr[3]==5)c[3]=c2[arr[3]];
 else if (sum<4)c[3]=c2[arr[3]];///ja (nak) pos ir"/0" un pats a vai b
