cb0e7750829644fd2636644bfd8f1b281aef7ad5
Ronny <ronaldsrundans@gmail.com>
2017-10-30 19:44:23 +0200

    Update cyk.cpp

--- automatu_teorija/cyk.cpp
+++ automatu_teorija/cyk.cpp
@@ -2,83 +2,67 @@
 #include <cstring>
 using namespace std;
 
-void searchString(char *c,int n,int arr[][4], char arrChar[][6])
+void searchString(char*c,int arr[][4], char arrChar[][6])
 {
-    int i,j,k;
+    int i,j,k,d,m;
 
     for(i=0;i<3;i++)///rinda
     {
         for(k=0;k<arr[i][0];k++)///elem-s p.k.
         {
+            d=arr[i][k+2]-arr[i][1+k];
+            m=0;
+
             for(j=arr[i][1+k];j<arr[i][k+2];j++)///elem-sak un beigas
             {
-                cout<<arrChar[i][j];
+
+//cout<<"c="<<c[m]<<endl;
+                //cout<<arrChar[i][j];
+                if(c[m]==arrChar[i][j])
+                {
+                    m++;
+                   // continue;
+                }
+                else break;
+
             }
+          //  cout<<d<<"m="<<m<<endl;
+            if(d==m)
+            {
+                cout<<"match"<<endl;
+            }
+           // cout<<endl;
+
         }
-        cout<<endl;
+
     }
 
 }
 
 int main()
 {
-int i,j,k, pos;
+
+int i,j,k,pos;
 int n=0;
 char arrChar[3][6]={{"SSAAB"},
                     {"AABa"},
                     {"BBBb"}};
 int arrInt[3][4]={{2,1,3,5},{2,1,3,4},{2,1,3,4}};
-char c[100];
+char c[3]="AB";
+//cout<<"c="<<c[0]<<endl;
+
+/*
 int arrInt1[4][4];///w11,w22,w33,w44
-for(i=0;i<5;i++)
-{
-  arrInt1[i][0]=0;
-}
-
-char word[5]="abbb";
-c[0]='a';
-searchString(c,1,arrInt,arrChar);
-/*for(k=0;k<4;k++)
-{
-word[k];
-
-}
-
-
-    for(i=0;i<4;i++)
+    for(i=0;i<5;i++)
     {
-        j=0;
-        while(arrChar[i][j]!=0)
-        {
-
-            c[0]=arrChar[i][j];
-            if(c[0]==word[k])
-            {
-                pos=arrInt1[k][0]+1;
-                n++;
-                //cout<<c<<n<<endl;
-                arrInt1[k][pos]=i;
-                arrInt1[k][0]=pos;
-            }
-            j++;
-        }
-
+        arrInt1[i][0]=0;
     }
-}
+
+    char word[5]="abbb";
 */
-for(i=0;i<4;i++)
-{
-    for(j=1;j<=arrInt1[i][0];j++)
-    {
-       // cout<<arrInt1[i][j]<<endl;
-        //cout<<arrChar[arrInt1[i][j]][0]<<endl;
-        c[0]=arrChar[arrInt1[i][j]][0];
-        c[1]=arrChar[arrInt1[i+1][j]][0];
-        cout<<c[0]<<c[1]<<endl;
-        ///mekle string AB un BB
-//        searchString(c,2,arrInt2);
-    }
-}
+
+searchString(c,arrInt,arrChar);
+
     cout << "Hello world!" << endl;
     return 0;
 }
