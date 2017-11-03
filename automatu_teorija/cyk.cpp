c475d52665ec962659fe85dc9e0e21fcb7ab5963
Ronny <ronaldsrundans@gmail.com>
2017-11-03 19:04:28 +0200

    Update cyk.cpp

--- automatu_teorija/cyk.cpp
+++ automatu_teorija/cyk.cpp
@@ -33,7 +33,7 @@ void searchString2(char*c,int arr[][4], char arrChar[][6],int **arrP, int n)
                 //arrP[n]=i;
                 pos=arrP[n][0]+1;
                // cout<<"pos="<<pos<<endl;
-                cout<<"match[n]="<<c<<endl;
+               /// cout<<"match[n]="<<c<<endl;
                 //<<n<<"i="<<i<<endl;
                 arrP[n][pos]=i;
                arrP[n][0]=pos;
@@ -49,7 +49,7 @@ int main()
     int m=0;
     int **marr;
     int *d;
-    char word[5]="aaab";
+    char word[30]="ababa";
     char c[2];
     int arrInt1[4][30];///rinda norada burtu no abbb
     for(y=0;y<4;y++)
@@ -68,8 +68,13 @@ int main()
                 {2,1,3,4}};
 
 //cout<<"c=j=0"<<c[0]<<endl;
+n=0;
+while(word[n]!=0)
+{
+    n++;
+}
 
-    n=4;
+  //  n=4;
     tmp=0;
 
 
@@ -141,98 +146,20 @@ d=new int[n];///diff
                             c[0]=arrChar[marr[d[k-1]+j-1][x]][0];
                             c[1]=arrChar[marr[d[i-k-1]+j+k-1][y]][0];
                             c[2]=0;
-                            cout<<c<<endl;
+                           // cout<<c<<endl;
                             searchString2(c,arrInt,arrChar,marr,r);
-
                          }
                      }
-
                  }
-               /* tmp++;
-                cout<<"tmp="<<tmp<<endl;
-                // cout<<"k="<<k<<endl;
-                 cout<<"ijk  "<<i<<" "<<j<<" "<<k<<endl;
-                 ///insert for for
-
-                    cout<<"c1=";
-                 cout<<"["<<k<<"] ["<<j<<"]"<<endl;
-                    cout<<"c2=";
-                 cout<<"["<<i-k<<"] ["<<j+k<<"]"<<endl;
-                 */
-
-///missing for for
-
-  /*  for(x=d[k-1]+j-1;x<marr[d[k-1]+j-1][0];x++)///rinda
-    {
-        for(y=1;y<=marr[x][0];y++)///colonna
-        {
-                    cout<<"x y"<<x<<"  "<<y<<"=>"<<arrChar[marr[x][y]][0]<<endl;
-        }
-
-    }
-
-                cout<<d[k-1]+j-1<<"   "<<d[i-k-1]+j+k-1<<endl;
-                 cout<<arrChar[d[k-1]+j-1][0]<<"  "<<arrChar[d[i-k-1]+j+k-1][0]<<endl;
-                  c[0]=arrChar[d[k-1]+j-1][0];
-                  c[1]=arrChar[d[i-k-1]+j+k-1][0];
-                  c[2]=0;
-                  cout<<c<<endl;
-                  searchString2(c,arrInt,arrChar,marr,d[i-1]+j-1);
-                //arrChar[d[k-1]+j-1][0]][0];
-               // cout<<c[0]<<endl;
-                 cout<<endl;
-                 cout<<marr[d[i-1]+j-1][0];
-                 break;*/
-                   //
-                   // c[1]=arrChar[d[i-k-1]+j+k-1][0];
-
-
-               /*  for()
-                 {
-                     for()
-                     {
-
-                            c[2]=0;
-                        cout<<c<<endl;
-//cout<<"rez rinda="<<<<endl;
-                        searchString2(c,arrInt,arrChar,marr,d[i-1]+j-1);
-                     }
-                 }*/
-
-
-                 ///fix search bigger arr
-                //
-
-
-  //cout<<"start="<<j<<"  l="<<i<<endl;
-
-
-        /*
-cout<<"l-p="<<i-k<<"  s+p="<<j+k<<endl;
-
-               //cout<<"l-p="<<i-k<< cout<<"s+p="<<j+k<<endl;
-
-               //cout<<"s="<<j<<"  "<<arrChar[j][0]<<arrChar[j+k+1][0]<<endl;
-
-
-                c[0]=0;///arrChar[i][0];
-                c[1]=0;
-                 c[2]=0;
-             //  searchString2(c,arrInt,arrChar,arrInt1,j);
-               for(y=0;y<3;y++)///izvedumi
-                {
-
-                }*/
             }
         }
-
     }
-    cout<<"end="<<endl;
-j=marr[9][0];
+   // cout<<"end="<<endl;
+j=marr[m-1][0];
 for(i=0;i<j;i++)
 {
-    cout<<marr[9][i]<<endl;
-    if(marr[9][i]==0)
+   // cout<<marr[9][i]<<endl;
+    if(marr[m-1][i]==0)
     {
         cout<<"pieder!"<<endl;
         break;
