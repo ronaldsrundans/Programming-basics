0d233203a7212bfdc09b54083d037297e3857e92
Ronny <ronaldsrundans@gmail.com>
2017-11-07 20:32:09 +0200

    Update cyk.cpp

--- automatu_teorija/cyk.cpp
+++ automatu_teorija/cyk.cpp
@@ -31,7 +31,7 @@ void Print(int n, int **arr)///funkcija izmet atkartojumus
     }
 }
 void searchString2(char*c,int arr[][20], char arrChar[][100],int **arrP, int n)
-{
+{///funkcija salidzina ar gramatiku
     int i,j,k,d,m,pos;
     for(i=0;i<28;i++)///rinda
     {
@@ -48,12 +48,11 @@ void searchString2(char*c,int arr[][20], char arrChar[][100],int **arrP, int n)
                 }
                 else break;
             }
-            if(d==m)
+            if(d==m)///atrada
             {
 
                 pos=arrP[n][0]+1;
-               // if(pos>1280000)  cout<<"pos"<<pos<<endl;
-                arrP[n][pos]=i;
+                arrP[n][pos]=i;///ieraksta vieta
                arrP[n][0]=pos;
             }
         }
@@ -61,16 +60,16 @@ void searchString2(char*c,int arr[][20], char arrChar[][100],int **arrP, int n)
 }
 int main()
 {
-    int i,j,k,l,pos,x,y,x1,y1,r;
+    int i,j,k,l,x,y,x1,y1,r;
     int n=0;
     int m=0;
     int **marr;
     int *d;
-    char word[30]="bbaa";
-///char word[30]="abbabbabbacabbaaaaabbaac"; ///pieder
+   /// char word[1000]="bbaa";
+char word[30]="abbabbabbacabbaaaaabbaac"; ///pieder L(G)
 
-///char word[30]="acaaaaaabbaaaaaaaaaaaaaaaaaa"; ///pieder
-///char word[30]="abbabbabbbbabbabbaaaaabb";///pieder
+///char word[30]="acaaaaaabbaaaaaaaaaaaaaaaaaa"; ///pieder L(G)
+///char word[30]="abbabbabbbbabbabbaaaaabb";///pieder L(G)
 
 
     char c[2];
@@ -105,7 +104,8 @@ char arrChar[28][100]={   {"SX3XTCXXHXCYYSUFEA2XGSCEQXBXM"},
                         {"HXU"}};
                         ///masivs ir saistits ar ieprieksejo masivu
                         ///rindas pirmais elements rada cik elementi sekos
-                        ///otra
+                        ///otrais un parejie ir elementu sakuma pozicijas
+                        ///{1,1,2} nozime: 1elements, sakas ar poziciju 1, beidzas ar poziciju 2 masiva
         int arrInt[28][20]={{14,1,3,5,7,9,11,13,15,17,19,21,23,25,27,28},
                             {12,1,3,5,7,9,11,13,15,17,19,21,23,25},
                             {4,1,3,5,7,9},
@@ -140,13 +140,11 @@ char arrChar[28][100]={   {"SX3XTCXXHXCYYSUFEA2XGSCEQXBXM"},
     {
         n++;
     }
-//    tmp=0;
     for(i=1;i<=n;i++)///sola garums
     {
         m=m+i;
     }
-    cout<<"m="<<m<<endl;
-    cout<<"n="<<n<<endl;
+
     marr=new int* [m];
     d=new int[n];///diff
     d[0]=0;
@@ -156,7 +154,7 @@ char arrChar[28][100]={   {"SX3XTCXXHXCYYSUFEA2XGSCEQXBXM"},
         d[i]=d[i-1]+l;
         l=l-1;
     }
-    for(i=0;i<m;i++)///viena rinda saja masiva atbilst vienai rutinai algoritma
+    for(i=0;i<m;i++)///viena rinda saja masiva atbilst vienai rutinai cyk algoritma
     {
        marr[i]=new int [1000000];
        marr[i][0]=0;
@@ -167,7 +165,7 @@ char arrChar[28][100]={   {"SX3XTCXXHXCYYSUFEA2XGSCEQXBXM"},
         c[1]=0;
         searchString2(c,arrInt,arrChar,marr,j);
     }
-    for(i=2;i<=n;i++)///sola garums
+    for(i=2;i<=n;i++)
     {
         for(j=1;j<=n-i+1;j++)///start of span
         {
@@ -175,7 +173,7 @@ char arrChar[28][100]={   {"SX3XTCXXHXCYYSUFEA2XGSCEQXBXM"},
             {
                 x1=marr[d[k-1]+j-1][0];///elementu sk rinda
                 y1=marr[d[i-k-1]+j+k-1][0];
-                r=d[i-1]+j-1;
+                r=d[i-1]+j-1;///rezultata rinda masiva marr
                 for(x=1;x<=x1;x++)
                 {
                     for(y=1;y<=y1;y++)
@@ -192,11 +190,12 @@ char arrChar[28][100]={   {"SX3XTCXXHXCYYSUFEA2XGSCEQXBXM"},
         }
     }
     j=marr[m-1][0];
+
     for(i=1;i<=j;i++)
     {
         if(marr[m-1][i]==0)
         {
-            cout<<"pieder!"<<endl;
+            cout<<"Ir uz generejams!"<<endl;
             break;
         }
     }
