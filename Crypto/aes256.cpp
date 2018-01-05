4a79773b2639e007b0282fbc247d38a66277b444
Ronny <ronaldsrundans@gmail.com>
2018-01-05 17:02:53 +0200

    Update aes256.cpp

--- Crypto/aes256.cpp
+++ Crypto/aes256.cpp
@@ -709,14 +709,14 @@ void shiftKey(int **arr, int nk, int rows)
 int main()
 {
     int i,j,k;
-    int s=192;///new key size
+    int s=128;///new key size
     int nk=s/32;
     int nb=4;
     int nr=nk+6;///number of rounds
     int idec=0;
     if(nk==4)
     {
-        idec=46;
+        idec=44;
     }
     else if(nk==6)
     {
@@ -731,9 +731,9 @@ int main()
     char *key16= new char [nk*8+1];
 //key16="2b7e151628aed2a6abf7158809cf4f3c";///test key 128
 //key16="603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4";///test key 256
-key16="000102030405060708090a0b0c0d0e0f1011121314151617";
+//key16="000102030405060708090a0b0c0d0e0f1011121314151617";
 //key16="000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f";
-  //  key16="000102030405060708090a0b0c0d0e0f";
+    key16="000102030405060708090a0b0c0d0e0f";
 
 //key16="8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b";
     int *arrk=new int[s];
